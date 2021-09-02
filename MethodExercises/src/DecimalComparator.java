5dec5cb0ca64621a497b9c023a54e8035925f5a1
Daniel Wallace <wallacedaniel999@gmail.com>
2021-09-01 18:53:38 -0700

    method challenges completed

--- MethodExercises/src/DecimalComparator.java
+++ MethodExercises/src/DecimalComparator.java
@@ -1,11 +1,30 @@
 public class DecimalComparator {
 
     public static void main(String[] args) {
-
+        System.out.println(areEqualByThreeDecimalPlaces(-1.54367, -1.5436798));
+        System.out.println(areEqualByThreeDecimalPlaces(3.1756, -3.175));
     }
 
-//    public static boolean areEqualByThreeDecimalPlaces(double , double ){
-//
-//
-//    }
+    public static boolean areEqualByThreeDecimalPlaces(double number1, double number2){
+
+        if (number1 < 0){
+            number1 = Math.floor(Math.abs(number1) * 1000) / 1000;
+            number1 = number1 *= -1;
+        } else {
+            number1 = Math.floor(number1 * 1000) / 1000;
+        }
+
+        if (number2 < 0){
+            number2 = Math.floor(Math.abs(number2) * 1000) / 1000;
+            number2 = number2 *= -1;
+        } else {
+            number2 = Math.floor(number2 * 1000) / 1000;
+        }
+
+        if (number1 == number2) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- MethodExercises/src/MinutesToYearsAndDays.java
+++ MethodExercises/src/MinutesToYearsAndDays.java
@@ -9,9 +9,12 @@ public class MinutesToYearsAndDays {
     public static void printYearsAndDays(long minutes){
         if(minutes < 0){
             System.out.println("Invalid Value");
-        } else{
-
-            //System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        } else {
+            int hours = (int) minutes / 60;
+            int days = hours / 24;
+            int years = days / 365;
+            int daysRemaining = days % 365;
+            System.out.println(minutes + " min = " + years + " y and " + daysRemaining + " d");
         }
     }
 }
