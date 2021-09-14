e41375f4fd2bcd366c52fdb99e903b27c64ed0d8
Daniel Wallace <wallacedaniel999@gmail.com>
2021-09-13 19:54:20 -0700

    control flow challenges

--- ControlFlowChallenges/src/com/company/FlourPacker.java
+++ ControlFlowChallenges/src/com/company/FlourPacker.java
@@ -2,22 +2,38 @@ package com.company;
 
 public class FlourPacker {
     public static void main(String[] args) {
+        System.out.println(canPack(-3,2,12));
         System.out.println(canPack(1, 0, 4));
         System.out.println(canPack(1, 0, 5));
         System.out.println(canPack(0, 5, 4));
         System.out.println(canPack(2,2,11));
-        System.out.println(canPack(-3,2,12));
+
     }
-    // big: 5  small: 1
+
     public static boolean canPack(int bigCount, int smallCount, int goal){
 
-        if(bigCount < 0 || smallCount < 0 || goal < 0) {
+        if(bigCount < 0 || smallCount < 0 || goal < 0 || bigCount * 5 + smallCount < goal) {
             return false;
         }
 
-        int bigCountKilos = 5 * bigCount;
+        int sum = 0;
 
+        for (int i = 0; i < bigCount; i++) {
+            sum += 5;
+            if (sum == goal) {
+                return true;
+            } else if (sum > goal) {
+                sum -= 5;
+                break;
+            }
+        }
 
-        return true;
+        for (int i = 0; i < smallCount; i++) {
+            sum += 1;
+            if (sum == goal) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- ControlFlowChallenges/src/com/company/LargestPrime.java
+++ ControlFlowChallenges/src/com/company/LargestPrime.java
@@ -3,11 +3,12 @@ package com.company;
 public class LargestPrime {
 
     public static void main(String[] args) {
+        System.out.println(getLargestPrime(7));
         System.out.println(getLargestPrime(21));
         System.out.println(getLargestPrime(217));
-        System.out.println(getLargestPrime(0));
+//        System.out.println(getLargestPrime(0));
         System.out.println(getLargestPrime(45));
-        System.out.println(getLargestPrime(-1));
+//        System.out.println(getLargestPrime(-1));
     }
 
     public static int getLargestPrime(int number){
@@ -16,7 +17,46 @@ public class LargestPrime {
             return -1;
         }
 
-        // if negative or no primes
+        for (int i = number; i > 0; i--){
+
+            if (number % i == 0){
+
+
+
+                System.out.println( i + " is a factor");
+
+                int j = 2;
+                boolean isPrime = false;
+                while (j <= i / 2) {
+                    // condition for nonprime number
+                    if (i % j == 0) {
+                        isPrime = true;
+                        System.out.println( j + " is a prime");
+                        //break;
+                    }
+
+                    ++j;
+                }
+
+//                for (int j = i; j >= 3; j--) {
+//
+//                    if (num % i == 0) {
+//                        flag = true;
+//                        break;
+//                    }
+//
+//                }
+
+
+
+//                for (int j = i; j > 2; j--) {
+//                    if (i % j == 0) {
+//                        return j;
+//                    }
+//                }
+//                return i;
+            }
+        }
         return -1;
     }
 }
